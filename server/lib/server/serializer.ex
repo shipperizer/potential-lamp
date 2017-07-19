f280f07f9ab988652fde9ba1defd0f6e15ad7283
shipperizer <alexcabb@gmail.com>
2017-07-19 13:39:35 +0100

    [++] guardian boilerplate

---
+++ server/lib/server/serializer.ex
@@ -0,0 +1,12 @@
+defmodule Server.GuardianSerializer do
+  @behaviour Guardian.Serializer
+
+  alias Server.Repo
+  alias Server.User
+
+  def for_token(user = %User{}), do: { :ok, "User:#{user.id}" }
+  def for_token(_), do: { :error, "Unknown resource type" }
+
+  def from_token("User:" <> id), do: { :ok, Repo.get(User, id) }
+  def from_token(_), do: { :error, "Unknown resource type" }
+end
